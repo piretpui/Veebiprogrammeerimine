41a69cca45afb97d0609a7ec2cd64910bdd835fb
Piret Puidak <piretpui@users.noreply.github.com>
2017-09-12 14:15:36 +0300

    Lisatud on esimene jupp PHP koodi

---
+++ esimene.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html> 
+<head>
+   <meta charset="utf-8">
+   <title>Piret Puidak veebiprogrammeerimine</title>
+</head>
+<body>
+   <h1>Piret Puidaku veebiprogrammeerimine</h1>
+   <p>See leht on loodud õppetöö raames ning ei sisalda mingit tõsiseltvõetavat sisu. </p>
+
+   <?php echo "See on minu esimene PHP abil väljastatud tekstijupp!";
+
+   ?> 
+</body>
+</html>
